e05d7d67e5f5fec04f8cf9eff3f2ac108149bfcb
Sandra Becker <sandra.becker1982@gmail.com>
2023-11-03 13:31:25 +0000

    js data loading

--- js/script.js
+++ js/script.js
@@ -17,104 +17,56 @@ var svg = d3.select("#graph")
 //  Data loading
 // --------------------------------------
 
-const data = d3.json("./exercise_fixed.json")    
+const data = d3.csv("./data/sankey_data.csv", d3.autoType)    
   .then(function(data){ 
 
-console.log(data);
+    let links = [];
 
-// --------------------------------------
-//  Tooltip
-// --------------------------------------
+    data.sort((b, a) => a["Decade"] - b["Decade"]);
 
-const tooltip = d3.tip()
-    .attr("class", "tip")
-    .html(
-      (event, d) => `
-      <div>
-        <b>Type</b> ${d.properties} // create a for each function
-      </div>`
+    data.map((row, i) => {
+      links.push({
+        source: row["Decade"],
+        target: row["Disaster"],
+        value: row["sum_costs"],
+        id: i
+      });
+    });
+
+    const nodes = Array.from(
+      new Set(links.flatMap((l) => [l.source, l.target])),
+      (name, id) => ({ name, id })
     );
 
-svg.call(tooltip); 
+    links.map((d) => {
+      d.source = nodes.find((e) => e.name === d.source).id;
+      d.target = nodes.find((e) => e.name === d.target).id;
+    });
+    
+    console.log(nodes);
+    console.log(links);
+    console.log(data);
+
+    let data_final = {nodes:nodes, links:links};
+    console.log(data_final);
 
 // --------------------------------------
 //  Scales
 // --------------------------------------
 
-let c = d3.scaleOrdinal()
-        .domain(["account",  "address", "person", "transaction"])
-        .range(["#06d6a0", "#662e9b", "#f20666", "#9EF211"]);
+let color = d3.scaleOrdinal()
+  .domain([
+    "Tropical Cyclone",
+    "Drought",
+    "Wildfire",
+    "Flooding",
+    "Winter Storm",
+    "Severe Storm"
+  ])
+  .range(["#A3AB78", "#BDE038", "#9FC131", "#005C53", "#042940", "#f20666"]);
 
-// --------------------------------------
-//  Drag function for nodes
-// --------------------------------------
 
-function drag (simulation) {
-  
-  function dragstarted(event, d) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-  
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-  }
-  
-  function dragended(event, d) {
-    if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-  
-  return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-}
 
-// --------------------------------------
-//  Force layout
-// --------------------------------------
 
-const simulation = d3.forceSimulation(data.nodes)
-	.force('charge', d3.forceManyBody()
-                     .strength(-50))
-	.force('center', d3.forceCenter(width / 2, height / 2))
-	.force('link', d3.forceLink(data.links)
-                   .id((d) => d.id))
-	.on('tick', ticked);
-
-function updateLinks() {
-	d3.select('svg')
-		.selectAll('line')
-		.data(data.links)
-		.join('line')
-    .attr("class", "line")
-		.attr('x1', d => d.source.x)
-		.attr('y1', d => d.source.y)
-		.attr('x2', d => d.target.x)
-		.attr('y2', d => d.target.y);
-}
-
-function updateNodes() {
-	d3.select('svg')
-		.selectAll('circle')
-		.data(data.nodes)
-		.join('circle')
-		.attr('r', 10)
-    .attr('cx', d => d.x)
-    .attr('cy', d => d.y)
-    .attr("fill", d => c(d.type))
-    .on("mouseover", tooltip.show)
-    .on("mouseout", tooltip.hide)
-    .call(drag(simulation));
-}
-
-function ticked() {
-	updateLinks()
-	updateNodes()
-}
 
 });
