342ecf033886e9b52cf061e89f2b9539df0ddcf4
Sandra Becker <sandra.becker1982@gmail.com>
2023-11-10 17:47:32 +0000

    final viz

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //  Margin and canvas
 // --------------------------------------
 
-const margin = { top: 30, right: 110, bottom: 20, left: 60 };
+const margin = { top: 30, right: 110, bottom: 20, left: 110 };
 
 const width = 1100 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
@@ -27,8 +27,8 @@ const data = d3.csv("./data/sankey_data.csv", d3.autoType)
 
     data.map((row, i) => {
       links.push({
-        source: row["Decade"],
-        target: row["Disaster"],
+        source: row["Disaster"], 
+        target: row["Decade"],
         value: row["sum_costs"],
         id: i
       });
@@ -69,24 +69,24 @@ let color = d3.scaleOrdinal()
   .range(["#A3AB78", "#BDE038", "#9FC131", "#005C53", "#042940", "#f20666"]);
 
 //Bar chart
-
   let y = d3.scaleBand()
   .domain([
-    "Tropical Cyclone",
-    "Drought",
     "Wildfire",
+    "Drought",
+    "Tropical Cyclone",
+    "Severe Storm", 
     "Flooding",
-    "Winter Storm",
-    "Severe Storm"
+    "Winter Storm"
   ])
-  .rangeRound([500, 0]);
+  .rangeRound([530, 30]);
 
   console.log(y.bandwidth());
 
 // --------------------------------------
 //  Sankey
 // --------------------------------------
-  const sankey = d3.sankey()
+  
+const sankey = d3.sankey()
   .nodeSort((a, b) => a.id - b.id)
   .nodeAlign(d3.sankeyCenter)
   .nodeId((d) => d.id)
@@ -104,17 +104,18 @@ console.log((sankey(data_final)));
 // --------------------------------------
 //  Data drawing 
 // --------------------------------------
-  
+
+//Nodes = rects
 svg
 .append("g")
 .selectAll("rect")
 .data((sankey(data_final)).nodes)
 .join("rect")
 .attr("width", (d) => d.y1 - d.y0)
-.attr("x", 350)
-.attr("fill", (d) => (d.x0 > width / 2 ? color(d.name) : "black"))
-.attr("opacity", (d) => (d.x0 > width / 2 ? 1 : 0))
-.attr("y", (d, i) => (y.bandwidth() * i) -50 )
+.attr("x", (d) => (d.x0 < width / 2 ? d.x0 : d.x0))
+.attr("fill", (d) => (d.x0 < width / 2 ? color(d.name) : "black"))
+.attr("opacity", (d) => (d.x0 < width / 2 ? 1 : 0))
+.attr("y", (d) => y(d.name))
 .attr("height", y.bandwidth())
 .on("mouseover", (e, d) => {
   d3.selectAll("path").style("opacity", (p) =>
@@ -126,21 +127,18 @@ svg
 })
 .transition()
 .delay(3000)
-.duration(2000)
-.attr("fill", (d) => (d.x0 > width / 2 ? color(d.name) : "black"))
+.duration(1500)
+.attr("fill", (d) => (d.x0 < width / 2 ? color(d.name) : "black"))
 .attr("height", (d) => d.y1 - d.y0)
-.attr("opacity", (d) => (d.x0 > width / 2 ? 1 : 0))
-.attr("width", (d) => (d.x0 > width / 2 ? d.x1 - d.x0 + 30 : 15))
+.attr("opacity", (d) => (d.x0 < width / 2 ? 1 : 0))
+.attr("width", (d) => (d.x0 < width / 2 ? d.x1 - d.x0 + 30 : 15))
 .attr("y", (d) => d.y0)
 .transition()
-.delay(1000)
-.duration(2000)
-.attr("x", (d) => (d.x0 > width / 2 ? d.x0 : d.x0 + 0))
-.transition()
-.delay(1000)
-.duration(1000)
-.attr("opacity", (d) => (d.x0 > width / 2 ? 1 : 1));
+.delay(500)
+.duration(1500)
+.attr("opacity", (d) => (d.x0 < width / 2 ? 1 : 1));
 
+//Links = path 
 const link = svg
     .append("g")
     .attr("fill", "none")
@@ -152,40 +150,40 @@ const link = svg
     .append("path")
     .attr("class", (d) => `trajectory_${d.id}`)
     .attr("d", d3.sankeyLinkHorizontal())
-    .attr("stroke", (d) => color(d.target.name))
+    .attr("stroke", (d) => color(d.source.name))
     .attr("stroke-opacity", 1)
     .attr("stroke-width", 0)
     .transition()
-    .delay((d) => 10000 + d.target.index * 1500)
+    .delay((d) => 7000 + d.source.id * 1500)
     .duration(1000)
     .attr("stroke-opacity", 1)
     .attr("stroke-width", (d) => Math.max(1, d.width));
 
-  const node = svg
+const node = svg
     .append("g")
     .selectAll("text")
     .data((sankey(data_final)).nodes)
     .join("text")
-    .attr("x", (d) => (d.x0 < width / 2 ? d.x1 - 20 : d.x0 + 50))
-    .attr("y", (d) => (d.y1 + d.y0) / 2)
+    .attr("x", (d) => (d.x0 > width / 2 ? d.x1 + 10 : d.x0 - 10))
+    .attr("y",  (d) => (d.x0 < width / 2 ? (y(d.name) + 50) : ((d.y1 + d.y0) / 2)))
     .attr("fill", "black")
     .attr("dy", "0.4em")
     .attr("text-anchor", (d) => (d.x0 < width / 2 ? "end" : "start"))
-    .attr("font-size", (d) => (d.x0 < width / 2 ? 0 : 0))
-    .attr("font-weight", (d) => (d.x0 < width / 2 ? 300 : 300))
+    .attr("font-size", (d) => (d.x0 > width / 2 ? 0 : 15))
+    .attr("font-weight", 300)
     .text((d) => d.name + "s")
     // .append("tspan")
     // .attr("fill-opacity", (d) => (d.x0 < width / 2 ? 0.5 : 0.5))
     // .attr("font-weight", 200)
     // .text((d) => `${format(d.value)}`)
     .transition()
-    .delay(8000)
-    .duration(500)
-    .attr("font-size", (d) => (d.x0 < width / 2 ? 0 : 15))
+    .delay(3000)
+    .duration(1500)
+    .attr("y", (d) => (d.y1 + d.y0) / 2)
     .transition()
-    .delay(1000)
-    .duration(500)
-    .attr("font-size", (d) => (d.x0 < width / 2 ? 15 : 15));
-
+    .delay(500)
+    .duration(1500)
+    .attr("y", (d) => (d.y1 + d.y0) / 2)
+    .attr("font-size", (d) => (d.x0 > width / 2 ? 15 : 15));
 
 });
